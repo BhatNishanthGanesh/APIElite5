2a850c03a52caf656422ae5bab4b4aafeedf4341
Prem-rao <raopremwaghmare@gmail.com>
2023-08-16 18:22:52 +0530

    A component to fetch a random question from the database

---
+++ src/pages/Questions.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useEffect,useState } from 'react'
+const Questions = () => {
+    const [data,setData]=useState("");
+
+    useEffect(()=>{
+        const fetchQuestion=()=>{
+            
+        }
+    })
+  return (
+    <>
+    </>
+  )
+}
+
+export default Questions
