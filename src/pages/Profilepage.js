2addd9b3a2a5b92c90d8bdd8277303d4ffd5a69f
Prem-rao <raopremwaghmare@gmail.com>
2023-08-17 19:11:48 +0530

    structuring profile page

---
+++ src/pages/Profilepage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Profilepage = () => {
+  return (
+    <div>
+        <div>
+            {/* Header */}
+        </div>
+        <div>
+            {/*Post request field*/}
+        </div>
+    </div>
+  )
+}
+
+export default Profilepage
