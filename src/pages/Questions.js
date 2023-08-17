4e792219b6e9cc0fe0e42cc5ecaa4df56dab1be9
Prem-rao <raopremwaghmare@gmail.com>
2023-08-17 14:55:48 +0530

    Added some more features to fetch next random question

--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -15,10 +15,31 @@ const Question=()=>{
   }, []);
 
   const handleClickedOption = (option) => {
+    for (const button of document.querySelectorAll('.opt')) {
+      button.disabled = true;
+    }
     if (option === questionData.correctOption) {
-      alert('Correct!');
+      alert('Correct ✔️')
     } else {
-      alert('Incorrect!');
+      alert('InCorrect ❌')
+    }
+  };
+  const [isNextQuestionAvailable, setIsNextQuestionAvailable] = useState(true);
+  const handleNextQuestionClick = () => {
+    for (const button of document.querySelectorAll('.opt')) {
+      button.disabled = false;
+    }
+    if (isNextQuestionAvailable) {
+      axios.get("http://localhost:4000/api/random-question")
+        .then((response) => {
+          setQuestionData(response.data);
+          setIsNextQuestionAvailable(true);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    } else {
+      alert('No more questions available');
     }
   };
 
@@ -26,24 +47,24 @@ const Question=()=>{
     <div>
       {questionData && (
         <>
-        <div>
-        <h3>{questionData.question}</h3>
+        <div className="d-flex">
         <ul className="">
+          <h3>{questionData.question}</h3>
           <li>
-            <button className='btn btn-outline-dark form-control' onClick={() => handleClickedOption(questionData.option1)}>{questionData.option1}</button> <br />
+            <button className='opt btn btn-outline-dark form-control' onClick={() => handleClickedOption(questionData.option1)}>{questionData.option1}</button> <br />
           </li>
           <li>
-            <button className='btn btn-outline-dark form-control' onClick={() => handleClickedOption(questionData.option2)}>{questionData.option2}</button> <br />       
+            <button className='opt btn btn-outline-dark form-control' onClick={() => handleClickedOption(questionData.option2)}>{questionData.option2}</button> <br />       
           </li>
           <li>
-            <button className='btn btn-outline-dark form-control' onClick={() => handleClickedOption(questionData.option3)}>{questionData.option3}</button>  <br />
+            <button className='opt btn btn-outline-dark form-control' onClick={() => handleClickedOption(questionData.option3)}>{questionData.option3}</button>  <br />
           </li>
-            <button className='btn btn-outline-dark form-control' onClick={() => handleClickedOption(questionData.option4)}>{questionData.option4}</button>
+            <button className='opt btn btn-outline-dark form-control' onClick={() => handleClickedOption(questionData.option4)}>{questionData.option4}</button>
         </ul>
         </div>
+          <button className="btn" onClick={handleNextQuestionClick}>Next Question</button>
         </>
       )}
-      
     </div>
   );
 };
