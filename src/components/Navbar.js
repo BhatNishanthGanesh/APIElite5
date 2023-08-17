375327d68daa95febeb7eb1a620b2ee259d8fb9a
dezuivere <shwetharao2003@gmail.com>
2023-08-17 20:40:19 +0530

    minor changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,27 @@
 import React from "react";
 
+
 function Navbar() {
   return (
     <div>
       <nav>
-        <div className="nav-wrapper">
-          <a href="#" className="brand-logo">
-            <strong>Logo</strong>
+        <div className=" blue-grey lighten-1 nav-wrapper">
+          <a  href="#"  className="black-text text-darken-2 brand-logo">
+            <strong  >Logo</strong>
           </a>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <ul id="nav-mobile" className=" right hide-on-med-and-down">
             <li>
-              <a href="#">About</a>
+              <strong><a className="black-text text-darken-2" href="#">API DOC</a></strong>
             </li>
             <li>
-              <a href="#">Login</a>
+              <strong><a className="black-text text-darken-2" href="#">ABOUT</a></strong>
+            </li>
+            
+            <li>
+             <strong> <a className="black-text text-darken-2" href="#">My Questions</a></strong>
             </li>
             <li>
-              <a href="#">Sign up</a>
+              <strong><a className="black-text text-darken-2" href="#"><i className="large material-icons">account_circle</i></a></strong>
             </li>
           </ul>
         </div>
