029c000c0f3ea3cf5e21a02f8718b93b67e0bfa7
dezuivere <shwetharao2003@gmail.com>
2023-08-17 22:20:48 +0530

    added css to the profile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,36 +10,77 @@ function Navbar() {
     <div>
       <nav>
         <div className=" blue-grey lighten-1 nav-wrapper">
-          <a  href="#"  className="black-text text-darken-2 brand-logo">
-            <strong  >Logo</strong>
+          <a href="#" className="black-text text-darken-2 brand-logo">
+            <strong>Logo</strong>
           </a>
           <ul id="nav-mobile" className=" right hide-on-med-and-down">
             <li>
-              <strong><a className="medium black-text text-darken-2" href="#">API DOC</a></strong>
+              <strong>
+                <a className="medium black-text text-darken-2" href="#">
+                  API DOC
+                </a>
+              </strong>
             </li>
             <li>
-              <strong><a className="black-text text-darken-2" href="#">ABOUT</a></strong>
+              <strong>
+                <a className="black-text text-darken-2" href="#">
+                  ABOUT
+                </a>
+              </strong>
             </li>
-            
+
             <li>
-             <strong> <a className="black-text text-darken-2" href="#">My Questions</a></strong>
+              <strong>
+                {" "}
+                <a className="black-text text-darken-2" href="#">
+                  My Questions
+                </a>
+              </strong>
             </li>
-            {
-            isAuthenticated?(
-                <>
-                <li>
-                <img src={user.picture} alt={user.name} />
-            </li>
-                <li>
-                    <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
-                Log Out
-                </button>
+            {isAuthenticated ? (
+              <>
+                <li >
+                  <img  src={user.picture} alt={user.name} 
+                    style={{
+                      width: '40px',
+                      height: '40px',
+                      borderRadius: '50%',
+                      marginRight: '10px',
+                    }}
+                  />
                 </li>
-                </>
-            ):(
-                <li><button onClick={() => loginWithRedirect()}>Log In</button>;</li>
-            )
-            }
+                <li>
+                  <button className="white btn"
+                    onClick={() =>
+                      logout({
+                        logoutParams: { returnTo: window.location.origin },
+                      })
+                    }
+                    style={{
+                      background: 'none',
+                      border: '1px solid #ccc',
+                      padding: '5px 10px',
+                      cursor: 'pointer',
+                    }}
+                  >
+                    <strong>Log Out</strong>
+                  </button>
+                </li>
+              </>
+            ) : (
+              <li>
+                <button className="white btn" onClick={() => loginWithRedirect()}
+                style={{
+                  background: 'none',
+                  border: '1px solid #ccc',
+                  padding: '5px 10px',
+                  cursor: 'pointer',
+                }}
+                >
+                  <strong>Log In</strong>
+                </button>
+              </li>
+            )}
           </ul>
         </div>
       </nav>
