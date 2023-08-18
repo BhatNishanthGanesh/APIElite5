515834f78edcb1da2c8846ce19812f027629120b
Prem-rao <raopremwaghmare@gmail.com>
2023-08-18 22:11:12 +0530

    removed unwanted components

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import {Link} from 'react-router-dom';
 
 function Header() {
-    const { user, isAuthenticated, isLoading } = useAuth0();
+    const { user} = useAuth0();
   return (
     <div>
       <nav>
