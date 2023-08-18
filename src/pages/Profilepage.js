f3479441904173b0c6b7d86ccb29c24dc663e2e1
dezuivere <shwetharao2003@gmail.com>
2023-08-18 21:48:19 +0530

    added header to the profile page

--- src/pages/Profilepage.js
+++ src/pages/Profilepage.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Header from '../components/Header'
+
 
 const Profilepage = () => {
   return (
     <div>
         <div>
-            {/* Header */}
+            <Header/>
         </div>
         <div>
             {/*Post request field*/}
