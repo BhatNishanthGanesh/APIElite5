b8767a63aec4118b6c202814afb4da1d672b7de4
BhatNishanthGanesh <nishanth.cs21@sahyadri.edu.in>
2023-08-22 23:02:21 +0530

    right/wrong_in_Questions

--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -1,3 +1,102 @@
+// Previous Code
+
+
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+
+// const Question = () => {
+//   const [questionData, setQuestionData] = useState(null);
+//   const [isAnswerCorrect, setIsAnswerCorrect] = useState(null);
+//   const [isNextQuestionAvailable, setIsNextQuestionAvailable] = useState(false);
+//   const [selectedOption, setSelectedOption] = useState(null);
+
+//   useEffect(() => {
+//     fetchQuestion();
+//   }, []);
+
+//   const fetchQuestion = () => {
+//     axios.get("https://api-stpn.onrender.com/api/random-question")
+//       .then((response) => {
+//         setQuestionData(response.data);
+//         setIsAnswerCorrect(null); // Clear previous answer feedback
+//         setIsNextQuestionAvailable(false); // Next question is not available until answered
+//       })
+//       .catch((error) => {
+//         console.error(error);
+//       });
+//   };
+
+//   const handleClickedOption = (option) => {
+//     if (isAnswerCorrect !== null) {
+//       // If answer is already answered, prevent re-clicking
+//       return;
+//     }
+
+//     setSelectedOption(option);
+//     if (option === questionData.correctOption) {
+//       setIsAnswerCorrect(true);
+//     } else {
+//       setIsAnswerCorrect(false);
+//     }
+
+//     setIsNextQuestionAvailable(true); // Enable next question after answering
+//   };
+
+//   const handleNextQuestionClick = () => {
+//     fetchQuestion();
+//     setSelectedOption(null);
+//   };
+
+//   return (
+//     <div className="container mt-5">
+//       {questionData && (
+//         <div className={`shading-animation`}>
+//           <div className="card">
+//             <div className="card-body">
+//               <h3 className="card-title">{questionData.question}</h3>
+//               <div className="d-flex flex-column">
+//                 {['option1', 'option2', 'option3', 'option4'].map((optionKey) => (
+//                   <button
+//                     key={optionKey}
+//                     className={`opt btn ${
+//                       selectedOption === questionData[optionKey]
+//                         ? isAnswerCorrect
+//                           ? 'selected-correct'
+//                           : 'selected-incorrect'
+//                         : ''
+//                     } mb-2`}
+//                     onClick={() => handleClickedOption(questionData[optionKey])}
+//                     disabled={isAnswerCorrect !== null}
+//                   >
+//                     {questionData[optionKey]}
+//                   </button>
+//                 ))}
+//               </div>
+//               {isAnswerCorrect !== null && (
+//                 <p className={`answer-feedback ${isAnswerCorrect ? 'text-success' : 'text-danger shake'}`}>
+//                   {isAnswerCorrect ? 'Correct ✔️' : 'Incorrect ❌'}
+//                 </p>
+//               )}
+//               <div className="d-flex">
+//                 <button
+//                   className="btn btn-primary"
+//                   onClick={handleNextQuestionClick}
+//                   disabled={!isNextQuestionAvailable}
+//                 >
+//                   <p className={`fw-bold zoom-in ${!isNextQuestionAvailable?'text-danger':'text-white'}`}>Next Question</p>
+//                 </button>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Question;
+
+
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
@@ -5,6 +104,7 @@ const Question = () => {
   const [questionData, setQuestionData] = useState(null);
   const [isAnswerCorrect, setIsAnswerCorrect] = useState(null);
   const [isNextQuestionAvailable, setIsNextQuestionAvailable] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
 
   useEffect(() => {
     fetchQuestion();
@@ -12,17 +112,23 @@ const Question = () => {
 
   const fetchQuestion = () => {
     axios.get("https://api-stpn.onrender.com/api/random-question")
-      .then((response) => {
-        setQuestionData(response.data);
-        setIsAnswerCorrect(null); // Clear previous answer feedback
-        setIsNextQuestionAvailable(false); // Next question is not available until answered
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+          .then((response) => {
+            setQuestionData(response.data);
+            setIsAnswerCorrect(null); // Clear previous answer feedback
+            setIsNextQuestionAvailable(false); // Next question is not available until answered
+          })
+          .catch((error) => {
+            console.error(error);
+          });
   };
 
   const handleClickedOption = (option) => {
+    if (isAnswerCorrect !== null) {
+      // If answer is already answered, prevent re-clicking
+      return;
+    }
+
+    setSelectedOption(option);
     if (option === questionData.correctOption) {
       setIsAnswerCorrect(true);
     } else {
@@ -34,65 +140,54 @@ const Question = () => {
 
   const handleNextQuestionClick = () => {
     fetchQuestion();
+    setSelectedOption(null);
+    setIsAnswerCorrect(null);
   };
 
   return (
     <div className="container mt-5">
       {questionData && (
-        <div className={` shading-animation`}>
-        <div className="card">
-          <div className="card-body">
-            <h3 className="card-title">{questionData.question}</h3>
-            <div className="d-flex flex-column">
-              <button
-                className="opt btn btn-outline-dark mb-2"
-                onClick={() => handleClickedOption(questionData.option1)}
-                disabled={isAnswerCorrect !== null}
-              >
-                {questionData.option1}
-              </button>
-              <button
-                className="opt btn btn-outline-dark mb-2"
-                onClick={() => handleClickedOption(questionData.option2)}
-                disabled={isAnswerCorrect !== null}
-              >
-                {questionData.option2}
-              </button>
-              <button
-                className="opt btn btn-outline-dark mb-2"
-                onClick={() => handleClickedOption(questionData.option3)}
-                disabled={isAnswerCorrect !== null}
-              >
-                {questionData.option3}
-              </button>
-              <button
-                className="opt btn btn-outline-dark mb-2"
-                onClick={() => handleClickedOption(questionData.option4)}
-                disabled={isAnswerCorrect !== null}
-              >
-                {questionData.option4}
-              </button>
-            </div>
-            {isAnswerCorrect !== null && (
-              <p className={`answer-feedback ${isAnswerCorrect ? 'text-success' : 'text-danger shake'}`}>
-                {isAnswerCorrect ? 'Correct ✔️' : 'Incorrect ❌'}
-              </p>
-            )}
-            <div className="d-flex">
-              <button
-                className="btn btn-primary"
-                onClick={handleNextQuestionClick}
-                disabled={!isNextQuestionAvailable}
-              >
-                <p className={`fw-bold zoom-in ${!isNextQuestionAvailable?'text-danger':'text-white'}`}>Next Question</p>
-              </button>
+        <div className={`shading-animation`}>
+          <div className="card">
+            <div className="card-body">
+              <h3 className="card-title">{questionData.question}</h3>
+              <div className="d-flex flex-column">
+                {['option1', 'option2', 'option3', 'option4'].map((optionKey) => (
+                  <button
+                    key={optionKey}
+                    className={`opt option-button mb-2 ${
+                      selectedOption === questionData[optionKey]
+                        ? isAnswerCorrect
+                          ? 'bg-success'
+                          : 'bg-danger'
+                        : ''
+                    } mb-2`}
+                    onClick={() => handleClickedOption(questionData[optionKey])}
+                    disabled={isAnswerCorrect !== null}
+                  >
+                    {questionData[optionKey]}
+                  </button>
+                ))}
+              </div>
+              {isAnswerCorrect !== null && (
+                <p className={`answer-feedback ${isAnswerCorrect ? 'text-success' : 'text-danger shake'}`}>
+                  {isAnswerCorrect ? 'Correct ✔️' : 'Incorrect ❌'}
+                </p>
+              )}
+              <div className="d-flex">
+                <button
+                  className="btn btn-primary"
+                  onClick={handleNextQuestionClick}
+                  disabled={!isNextQuestionAvailable}
+                >
+                  <p className={`fw-bold zoom-in ${!isNextQuestionAvailable?'text-danger':'text-white'}`}>Next Question</p>
+                </button>
+              </div>
             </div>
           </div>
         </div>
-        </div>
       )}
     </div>
-
   );
 };
 
